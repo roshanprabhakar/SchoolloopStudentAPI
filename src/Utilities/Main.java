75512c621bff1ff0c9fed4dd4cd165eb5850ae0f
Roshan Prabhakar <roshan@aprabhakar.com>
2019-12-13 21:36:22 -0800

    added template usage for APLAC

--- src/Utilities/Main.java
+++ src/Utilities/Main.java
@@ -1,5 +1,6 @@
 package Utilities;
 
+import classtemplates.APLAC;
 import classtemplates.PhysicsAP;
 
 import java.util.HashMap;
@@ -9,15 +10,19 @@ public class Main {
     public static final String USERNAME = "Your username";
     public static final String PASSWORD = "Your password";
 
+    // example usage, API purposed for providing data to create more methods
     public static void main(String[] args) throws InterruptedException {
 
         //Scrapes data from schoolloop to GRADES
 //        Scraper.writeMap(Scraper.scrapeGradeData(USERNAME, PASSWORD), "GRADES.txt");
 
         HashMap<String, Block> blocks = Scraper.readGradeData("GRADES.txt"); //This HashMap contains all the data from GRADES in the form of <Block name, Block Object>
+        //The names contained in this map are exactly as they appear on the school loop dashboard
 
-        // example usage, API purposed for providing data to create more methods
         PhysicsAP physics = new PhysicsAP(blocks.get("Physics AP"));
-        physics.analyze();
+//        physics.analyze();
+
+        APLAC aplac = new APLAC(blocks.get("AP Lang/Comp"));
+        aplac.analyze();
     }
 }
---
+++ src/classtemplates/APLAC.java
@@ -0,0 +1,18 @@
+package classtemplates;
+
+import Utilities.Block;
+import Utilities.WeightedBlock;
+
+public class APLAC extends WeightedBlock {
+
+    public APLAC(Block blockData) {
+        super(blockData.getEntries(), blockData.getClassName());
+        defineCategories();
+    }
+
+    @Override
+    public void defineCategories() {
+        categories.put("Mastery", 0.8);
+        categories.put("Practice", 0.2);
+    }
+}
